8f2c5a210e25212d1ff0a80f796564d5a42283d6
Laksh Dhamija <laksh.dhamija@gmail.com>
2020-07-30 22:32:28 +0530

    Add rotation feature for the Disk

--- src/App.js
+++ src/App.js
@@ -5,46 +5,85 @@ import Albums from './components/albums';
 import Artists from './components/artists';
 import Settings from './components/settings';
 import Disk from './components/disk';
+import ZingTouch from 'zingtouch';
 
 class App extends Component {
   state = {
-    menuDisp: true,
+    mainMenuDisp: true,
     albumsDisp: false,
     songsDisp: false,
     artistsDisp: false,
-    settingsDisp: false
-  };  
-  componentDidMount(){
+    settingsDisp: false,
+    degree: 0
+  };
+  componentDidMount() {
     this.rotation();
   }
-  rotation = async function() {
-    var disk = document.getElementsByClassName("Disk");
-    var activeRegion = ZingTouch.Region(containerElement[0]); //defining active region
-    activeRegion.bind(containerElement[0], "rotate", (event) => {
-      event.stopPropagation();
-
-      //comparing angle with previous angle in state
-      if (
-        event.detail.angle - this.state.angle > 15 ||
-        event.detail.angle - this.state.angle < -15
-      ) {
-        if (event.detail.distanceFromLast > 0) {
-          this.toggleClockwise();
-        } else if (event.detail.distanceFromLast < 0) {
-          this.toggleAntiClockwise();
+  rotation = function () { // Function to make our disk rotate
+    var disk = document.getElementsByClassName("Disk")[0];
+    var turnRegion = ZingTouch.Region(disk);
+    turnRegion.bind(disk, 'rotate', (e) => {
+      if (e.detail.angle - this.state.degree > 15 || e.detail.angle - this.state.degree < -15) {
+        if (e.detail.distanceFromLast > 0) {
+          this.rotateRight();
+        } else if (e.detail.distanceFromLast < 0) {
+          this.rotateLeft();
         }
         this.setState({
-          angle: event.detail.angle,
+          degree: e.detail.angle
         });
       }
     });
   };
+  rotateRight =  function() {
+    var albums = document.getElementsByClassName("albums")[0];
+    var songs = document.getElementsByClassName("songs")[0];
+    var artists = document.getElementsByClassName("artists")[0];
+    var settings = document.getElementsByClassName("settings")[0];
+    if (this.state.mainMenuDisp) {
+      if (albums.classList.contains('highlight')) {
+        albums.classList.remove('highlight');
+        songs.classList.add('highlight');
+      } else if (songs.classList.contains('highlight')) {
+        songs.classList.remove('highlight');
+        artists.classList.add('highlight');
+      } else if (artists.classList.contains('highlight')) {
+        artists.classList.remove('highlight');
+        settings.classList.add('highlight');
+      } else if (settings.classList.contains('highlight')) {
+        settings.classList.remove('highlight');
+        albums.classList.add('highlight');
+      }
+    }
+  };
+
+  rotateLeft = function() {
+    var albums = document.getElementsByClassName("albums")[0];
+    var songs = document.getElementsByClassName("songs")[0];
+    var artists = document.getElementsByClassName("artists")[0];
+    var settings = document.getElementsByClassName("settings")[0];
+    if (this.state.mainMenuDisp) {
+      if (albums.classList.contains('highlight')) {
+        albums.classList.remove('highlight');
+        settings.classList.add('highlight');
+      } else if (settings.classList.contains('highlight')) {
+        settings.classList.remove('highlight');
+        artists.classList.add('highlight');
+      } else if (artists.classList.contains('highlight')) {
+        artists.classList.remove('highlight');
+        songs.classList.add('highlight');
+      } else if (songs.classList.contains('highlight')) {
+        songs.classList.remove('highlight');
+        albums.classList.add('highlight');
+      }
+    }
+  };
   render() {
-    const {menuDisp, albumsDisp, songsDisp, artistsDisp, settingsDisp} = this.state;
+    const { mainMenuDisp, albumsDisp, songsDisp, artistsDisp, settingsDisp } = this.state;
     return (
       <div className="App">
         <div className="main">
-          {menuDisp && <Display />}
+          {mainMenuDisp && <Display />}
           {albumsDisp && <Albums />}
           {artistsDisp && <Artists />}
           {settingsDisp && <Settings />}
