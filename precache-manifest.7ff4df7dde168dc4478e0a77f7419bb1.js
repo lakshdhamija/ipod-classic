9b93afb7d75eca2e67b7bb37bdf0abe5fc7c2729
Laksh Dhamija <laksh.dhamija@gmail.com>
2020-08-04 19:45:01 +0530

    Updates

---
+++ precache-manifest.7ff4df7dde168dc4478e0a77f7419bb1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "216f500ea1583df17b276277cefebfc6",
+    "url": "/ipod-classic/index.html"
+  },
+  {
+    "revision": "df871e70700fd441b964",
+    "url": "/ipod-classic/static/css/main.c086ac40.chunk.css"
+  },
+  {
+    "revision": "c8f007af03f5b074cdfb",
+    "url": "/ipod-classic/static/js/2.e433b084.chunk.js"
+  },
+  {
+    "revision": "3b6069e01f0db714f9e787b4dbe9775c",
+    "url": "/ipod-classic/static/js/2.e433b084.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df871e70700fd441b964",
+    "url": "/ipod-classic/static/js/main.52d658f3.chunk.js"
+  },
+  {
+    "revision": "deec2c51680d68e75f88",
+    "url": "/ipod-classic/static/js/runtime-main.0545413d.js"
+  },
+  {
+    "revision": "2927e39c6a21eb45216e8664b6cf5e1d",
+    "url": "/ipod-classic/static/media/main_bg.2927e39c.png"
+  }
+]);
